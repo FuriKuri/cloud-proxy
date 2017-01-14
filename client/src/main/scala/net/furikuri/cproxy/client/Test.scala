bee81d8803ccf039c2250f8a0e4d0ccff6b305cf
Theo Pack Test <tf.pack@gmail.com>
2017-01-14 22:22:46 +0100

    Add two hello world files

---
+++ client/src/main/scala/net/furikuri/cproxy/client/Test.scala
@@ -0,0 +1,5 @@
+package net.furikuri.cproxy.client
+
+object Hi {
+  def main(args: Array[String]) = println("Hi!")
+}
---
+++ server/src/main/scala/net/furikuri/cproxy/server/Test.scala
@@ -0,0 +1,5 @@
+package net.furikuri.cproxy.server
+
+object Hi {
+  def main(args: Array[String]) = println("Hi!")
+}
