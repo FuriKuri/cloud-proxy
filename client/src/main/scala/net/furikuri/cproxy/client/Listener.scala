9f2cc692f4fac91b84f35b31baba7d5b6cae6a38
Theo Pack Test <tf.pack@gmail.com>
2017-01-16 12:51:33 +0100

    Add listener

---
+++ client/src/main/scala/net/furikuri/cproxy/client/Listener.scala
@@ -0,0 +1,27 @@
+package net.furikuri.cproxy.client
+
+
+import java.net.InetSocketAddress
+
+import akka.actor.{ActorLogging, Actor}
+import akka.io.Tcp.{CommandFailed, Connect, Connected, ConnectionClosed}
+
+class Listener extends Actor with ActorLogging {
+  var clients = Set(
+    context.actorOf(TcpClient.props(new InetSocketAddress("google.de", 80), self))
+  )
+
+  override def receive: Receive = {
+    case CommandFailed(conn: Connect) =>
+      log.info(s"${sender}: CommandFaild ${conn}")
+      context stop self
+
+    case Connected(r, l) =>
+      log.info(s"${sender}: The local ${l} connected to the remote ${r}")
+
+    case cc: ConnectionClosed =>
+      log.info(s"${sender}: ${cc}")
+      clients -= sender
+      if (clients.isEmpty) context stop self
+  }
+}
