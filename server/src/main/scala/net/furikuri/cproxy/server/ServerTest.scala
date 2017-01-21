94d091b2fece415785e866c02aaf44fd9458b219
Theo Pack Test <tf.pack@gmail.com>
2017-01-21 11:42:39 +0100

    Change proxy port to 3000

--- server/src/main/scala/net/furikuri/cproxy/server/ServerTest.scala
+++ server/src/main/scala/net/furikuri/cproxy/server/ServerTest.scala
@@ -4,6 +4,6 @@ import akka.actor.{ActorSystem, Props}
 
 object ServerTest extends App {
   val system = ActorSystem("TcpServerTest")
-  val endpointServer = system.actorOf(Props(new ProxyServer(8080)))
+  val endpointServer = system.actorOf(Props(new ProxyServer(3000)))
   val clientProxyServer = system.actorOf(Props(new ClientProxy(4444, endpointServer)))
 }
